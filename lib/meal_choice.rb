2d178db1bc2ffdcc82b192bfe2b7a084f97dccef
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-05 07:19:20 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
-def meal_choice{veg1, veg2, protein}
+def meal_choice(veg1, veg2, protein)
   puts "What a nutritious meal!"
   puts "A plate of #{protiein} with #{veg1} and #{veg2}."
 end
-meal_choice{"carrots", "potatos", "steak"}
+meal_choice("carrots", "potatos", "steak")
