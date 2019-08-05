cd869e9dfca91a6901a54c633c66bff0fd3bae2c
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-05 06:41:51 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,5 @@
-# Your code here
+def meal_choice{veg1, veg2, protein}
+  puts "What a nutritious meal!"
+  puts "A plate of #{protiein} with #{veg1} and #{veg2}."
+end
+meal_choice{"carrots", "potatos", "steak"}
